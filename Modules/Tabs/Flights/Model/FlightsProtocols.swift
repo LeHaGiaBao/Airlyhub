1bdc917bbbab468c52c96ac2f30e1efce29aa6c9
LeHaGiaBao <lehagiabao0205@gmail.com>
2026-05-30 18:22:40 +0700

    chore: refactor to adapt VIPER

--- Modules/Tabs/Flights/Model/FlightsProtocols.swift
+++ Modules/Tabs/Flights/Model/FlightsProtocols.swift
@@ -8,13 +8,17 @@
 import Foundation
 
 protocol FlightsViewProtocol: AnyObject {
-    
+    func setLocation(_ location: LocationResult, for field: LocationField)
+    func showLocationError(_ error: LocationServiceError)
 }
 
 protocol FlightsPresenterProtocol {
     func viewDidLoad()
+    func fetchMyLocation(for field: LocationField)
 }
 
-protocol FlightsInteractorProtocol {}
+protocol FlightsInteractorProtocol {
+    func fetchCurrentLocation(completion: @escaping (Result<LocationResult, LocationServiceError>) -> Void)
+}
 
 protocol FlightsRouterProtocol: AnyObject {}
